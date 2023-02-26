5e9d589645fd535c625608475752ecf0aa524a31
Quitianote <santiago.quitian@udea.edu.co>
2023-02-26 13:22:55 -0500

    ejercicio 2 terminado

--- lab_1/Ejercicio_2/main.cpp
+++ lab_1/Ejercicio_2/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
 
-    int N;
+    int N = 0;
     int cont = 0;
 
     cout << "Ingrese la cantidad a determinar: "; cin >> N;
@@ -27,52 +27,79 @@ int main(){
     cout << "20000: " << cont << endl;
     cont = 0;
 
-    while(N >= 20000){
+    while(N >= 10000){
         cont ++;
-        N -= 20000;
+        N -= 10000;
 
     }
 
-    cout << "20000: " << cont << endl;
+    cout << "10000: " << cont << endl;
     cont = 0;
 
-    while(N >= 20000){
+    while(N >= 5000){
         cont ++;
-        N -= 20000;
+        N -= 5000;
 
     }
 
-    cout << "20000: " << cont << endl;
+    cout << "5000: " << cont << endl;
     cont = 0;
 
-    while(N >= 20000){
+    while(N >= 2000){
         cont ++;
-        N -= 20000;
+        N -= 2000;
 
     }
 
-    cout << "20000: " << cont << endl;
+    cout << "2000: " << cont << endl;
     cont = 0;
 
-    while(N >= 20000){
+    while(N >= 1000){
         cont ++;
-        N -= 20000;
+        N -= 1000;
 
     }
 
-    cout << "20000: " << cont << endl;
+    cout << "1000: " << cont << endl;
     cont = 0;
 
-    while(N >= 20000){
+    while(N >= 500){
         cont ++;
-        N -= 20000;
+        N -= 500;
 
     }
 
-    cout << "20000: " << cont << endl;
+    cout << "500: " << cont << endl;
     cont = 0;
 
-    for
+    while(N >= 200){
+        cont ++;
+        N -= 200;
+
+    }
+
+    cout << "200: " << cont << endl;
+    cont = 0;
+
+    while(N >= 100){
+        cont ++;
+        N -= 100;
+
+    }
+
+    cout << "100: " << cont << endl;
+    cont = 0;
+
+    while(N >= 50){
+        cont ++;
+        N -= 50;
+
+    }
+
+    cout << "50: " << cont << endl;
+
+    cout << "Faltante: " << N << endl;
+
 
     return 0;
 }
