56263300826047f834c5ad3ee5cbf852ba67dad9
Quitianote <santiago.quitian@udea.edu.co>
2023-02-26 13:03:05 -0500

    ejerciocio 2

---
+++ lab_1/Ejercicio_2/main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int N;
+    int cont = 0;
+
+    cout << "Ingrese la cantidad a determinar: "; cin >> N;
+
+    while(N >= 50000){
+        cont ++;
+        N -= 50000;
+
+    }
+
+    cout << "50000: " << cont << endl;
+    cont = 0;
+
+    while(N >= 20000){
+        cont ++;
+        N -= 20000;
+
+    }
+
+    cout << "20000: " << cont << endl;
+    cont = 0;
+
+    while(N >= 20000){
+        cont ++;
+        N -= 20000;
+
+    }
+
+    cout << "20000: " << cont << endl;
+    cont = 0;
+
+    while(N >= 20000){
+        cont ++;
+        N -= 20000;
+
+    }
+
+    cout << "20000: " << cont << endl;
+    cont = 0;
+
+    while(N >= 20000){
+        cont ++;
+        N -= 20000;
+
+    }
+
+    cout << "20000: " << cont << endl;
+    cont = 0;
+
+    while(N >= 20000){
+        cont ++;
+        N -= 20000;
+
+    }
+
+    cout << "20000: " << cont << endl;
+    cont = 0;
+
+    while(N >= 20000){
+        cont ++;
+        N -= 20000;
+
+    }
+
+    cout << "20000: " << cont << endl;
+    cont = 0;
+
+    for
+
+    return 0;
+}
