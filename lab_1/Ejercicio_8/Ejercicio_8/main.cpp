9035c6fbaf040353f9f9c0519268d59cd680d8e0
Quitianote <santiago.quitian@udea.edu.co>
2023-03-01 21:48:54 -0500

    empece el ejercicio 8

---
+++ lab_1/Ejercicio_8/Ejercicio_8/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a = 0;
+    int b = 0;
+    int c = 0;
+
+
+    cout << "Ingrese a: "<< cin >> a << endl;
+    cout << "Ingrese b: "<< cin >> b << endl;
+    cout << "Ingrese c: "<< cin >> c << endl;
+
+    for( true, )
+
+    return 0;
+}
