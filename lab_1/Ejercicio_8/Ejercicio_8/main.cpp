bebc9065b61637b578a05b2bbecee16b5334d7ca
Quitianote <santiago.quitian@udea.edu.co>
2023-03-04 09:12:25 -0500

    ejercicio 8 resuelto

--- lab_1/Ejercicio_8/Ejercicio_8/main.cpp
+++ lab_1/Ejercicio_8/Ejercicio_8/main.cpp
@@ -2,18 +2,47 @@
 
 using namespace std;
 
+int ciclo(int a, int b, int c);
+
 int main(){
 
     int a = 0;
     int b = 0;
     int c = 0;
 
+    cout << "Ingrese a: "; cin >> a;
+    cout << "Ingrese b: "; cin >> b;
+    cout << "Ingrese c: "; cin >> c;
 
-    cout << "Ingrese a: "<< cin >> a << endl;
-    cout << "Ingrese b: "<< cin >> b << endl;
-    cout << "Ingrese c: "<< cin >> c << endl;
-
-    for( true, )
+    ciclo( a, b, c);
 
     return 0;
 }
+
+int ciclo( int a, int b, int c){
+
+    int aux = 1;
+    int resul = 0;
+    for(; aux < c; aux ++)
+        if (aux%a == 0){
+            cout << aux << "+";
+            resul += aux;
+        }
+
+    aux = 1;
+
+    for(; aux < c; aux ++)
+        if (aux%b == 0 && aux%a != 0){
+            cout << aux;
+            if (aux + b < c && (aux + b)%a != 0){
+                cout << "+";
+            }
+            resul += aux;
+        }
+
+    cout << "=" << resul << endl;
+
+
+    return resul;
+}
+
