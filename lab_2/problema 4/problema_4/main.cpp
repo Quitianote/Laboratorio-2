c377cc57f3f0450ca2c0cf87e339afb1825b2fab
Quitianote <santiago.quitian@udea.edu.co>
2023-03-14 19:31:19 -0500

    problema 4 completada

--- lab_2/problema 4/problema_4/main.cpp
+++ lab_2/problema 4/problema_4/main.cpp
@@ -1,93 +1,36 @@
 #include <iostream>
 #include <conio.h>
-#include <string.h>
 
 using namespace std;
 
-int conv(char arre);
-
-int main(){
-
-    int arre[10];
-
-    cin.getline(arre, 10, '\n');
-
-    conv(arre);
-
-    return 0;
-}
-
-
-int conv(char arre){
-
-    int rec = 0;
-    int a = 0;
-    int b = 0;
-    int lon = sizeof(arre)-1;
-
-    for(;rec < lon; rec++){
-        a = arre[rec] - 48;
-        b = (b*10) + a;
-    }
-
-    cout << b;
-
-
-}
-
-/*
-
-#include <iostream>
-#include <conio.h>
-#include <string.h>
-
-using namespace std;
-
-int conv(char arre);
+int conv(char arre[], int lon);
 
 int main(){
 
     int num = 0;
-    char arre[];
+    int lon = 0;
+    char arre[] = "123";
+    lon = sizeof(arre)-1;
 
-    cin >>
-
-    num = conv(arre);
+    conv(arre, lon);
 
     return 0;
 }
 
 
-}
 
-int conv(char *arre){
+int conv(char arre[], int lon){//funcion del problema 4, la funcion "main"solo la use para hacer pruebas
 
     int rec = 0;
     int a = 0;
     int resul = 0;
-    int lon = sizeof(arre)-1;
+
 
     for(;rec < lon; rec++){
         a = arre[rec] - 48;
         resul = (resul*10) + a;
     }
 
-    cout << resul;
-
     return resul;
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
